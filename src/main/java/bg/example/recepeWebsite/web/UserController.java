954cce86c5d8e2b7c2a557fc73f18779e38c931f
98AnnaM <anna.mileva92@gmail.com>
2023-11-14 21:12:19 +0200

    index page

--- src/main/java/bg/example/recepeWebsite/web/UserController.java
+++ src/main/java/bg/example/recepeWebsite/web/UserController.java
@@ -91,7 +91,7 @@ public class UserController {
     @GetMapping("/{id}/addedRecipes")
     public String addedRecipes(@PathVariable Long id,
                                Model model,
-                               @PageableDefault(sort = "name", direction = Sort.Direction.ASC, page = 0, size = 1) Pageable pageable) {
+                               @PageableDefault(sort = "name", direction = Sort.Direction.ASC, page = 0, size = 12) Pageable pageable) {
 
         Page<RecipeViewModel> recipes = recipeService.findAllByUserId(id, pageable);
         model.addAttribute("recipes", recipes);
@@ -106,7 +106,7 @@ public class UserController {
     @GetMapping("/{id}/addedPictures")
     public String addedPictures(@PathVariable Long id,
                                Model model,
-                               @PageableDefault(page = 0, size = 1) Pageable pageable) {
+                               @PageableDefault(page = 0, size = 12) Pageable pageable) {
 
         String principalUserName = userService.findById(id).getUsername();
         Page<PictureViewModel> pictures =  pictureService.findAllPictureViewModelsByUsername(principalUserName ,pageable);
