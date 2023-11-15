4fe137d81fea2523d5ef2826ec2dd06a44d38cbf
98AnnaM <anna.mileva92@gmail.com>
2023-11-15 23:47:07 +0200

    add to favorite logic

---
+++ src/main/java/bg/example/recepeWebsite/scheduling/ScheduleTasks.java
@@ -0,0 +1,21 @@
+package bg.example.recepeWebsite.scheduling;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ScheduleTasks {
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(ScheduleTasks.class);
+    private final ReportService reportService;
+
+    public ScheduleTasks(ReportService reportService) {
+        this.reportService = reportService;
+    }
+
+    @Scheduled(cron = "*/10 * * * * *")
+    private void generateDailyReport(){
+        log.info("Start creating report");
+        reportService.generateDailyReport();
+        log.info("End report - report was sent");
+    }
+}
