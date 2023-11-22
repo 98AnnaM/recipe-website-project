2033c51d4b6a6150b506eb917c890a627274106c
98AnnaM <anna.mileva92@gmail.com>
2023-11-22 18:36:43 +0200

    implement forgot password logic

--- src/main/java/bg/example/recepeWebsite/service/SecureTokenService.java
+++ src/main/java/bg/example/recepeWebsite/service/SecureTokenService.java
@@ -3,14 +3,22 @@ package bg.example.recepeWebsite.service;
 import bg.example.recepeWebsite.model.entity.SecureTokenEntity;
 import bg.example.recepeWebsite.model.entity.UserEntity;
 import bg.example.recepeWebsite.repository.SecureTokenRepository;
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.security.crypto.keygen.BytesKeyGenerator;
+import org.springframework.security.crypto.keygen.KeyGenerators;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
 public class SecureTokenService {
+
+    private static final BytesKeyGenerator DEFAULT_TOKEN_GENERATOR = KeyGenerators.secureRandom(15);
+    private static final Charset US_ASCII = StandardCharsets.US_ASCII;
 
     private final SecureTokenRepository secureTokenRepository;
     private final UserService userService;
@@ -21,7 +29,7 @@ public class SecureTokenService {
     }
 
     private SecureTokenEntity createSecureToken(UserEntity user) {
-        String tokenValue = UUID.randomUUID().toString();
+        String tokenValue = new String(Base64.encodeBase64URLSafe(DEFAULT_TOKEN_GENERATOR.generateKey()), US_ASCII);
         SecureTokenEntity secureTokenEntity = new SecureTokenEntity();
         secureTokenEntity.setToken(tokenValue);
         secureTokenEntity.setExpireAt(LocalDateTime.now().plusMinutes(30));
