366ece3bb9370af8724d1ef71cc16cf9279cec17
98AnnaM <anna.mileva92@gmail.com>
2023-11-27 16:48:49 +0200

    Include green mail in the tests

--- src/test/java/bg/example/recepeWebsite/web/UserRegisterControllerTest.java
+++ src/test/java/bg/example/recepeWebsite/web/UserRegisterControllerTest.java
@@ -1,7 +1,9 @@
 package bg.example.recepeWebsite.web;
 
+import bg.example.recepeWebsite.model.entity.UserEntity;
 import bg.example.recepeWebsite.repository.SecureTokenRepository;
 import bg.example.recepeWebsite.repository.UserRepository;
+import bg.example.recepeWebsite.service.UserService;
 import com.icegreen.greenmail.util.GreenMail;
 import com.icegreen.greenmail.util.GreenMailUtil;
 import com.icegreen.greenmail.util.ServerSetup;
@@ -13,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
@@ -20,7 +23,11 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import jakarta.mail.internet.MimeMessage;
 import jakarta.servlet.http.Cookie;
+import org.springframework.validation.MessageCodesResolver;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import java.util.Locale;
+import java.util.Optional;
 
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
@@ -50,6 +57,8 @@ class UserRegisterControllerTest {
     private SecureTokenRepository secureTokenRepository;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserService userService;
 
     @BeforeEach
     void setup() {
@@ -79,8 +88,6 @@ class UserRegisterControllerTest {
     private static final String TEST_USER_EMAIL = "user@example.com";
     private static final String TEST_USER_PASSWORD = "12345";
     private static final String BEST_COOK_EMAIL = "no-reply@bestCook.com";
-    private static final String VERIFICATION_SUBJECT = "registration_subject";
-    private static final String TEMPLATE_LOCATION = "email/validation";
     private static final String BASE_URL = "http://localhost:8080";
 
     @Test
@@ -106,58 +113,48 @@ class UserRegisterControllerTest {
 
         Assertions.assertTrue(GreenMailUtil
                 .getBody(verificationEmail)
-                .contains("Anna Petrova"));
-        Assertions.assertEquals(1, verificationEmail.getAllRecipients().length);
-        Assertions.assertEquals("anna@example.com", verificationEmail
-                .getAllRecipients()[0].toString());
+                .contains(TEST_USER_FIRSTNAME + " " + TEST_USER_LASTNAME));
 
-//        ArgumentCaptor<AccountVerificationEmailContext> emailContextCaptor =
-//                ArgumentCaptor.forClass(AccountVerificationEmailContext.class);
-//
-//        verify(mockEmailService).sendEmail(emailContextCaptor.capture());
-//
-//        AccountVerificationEmailContext capturedEmailContext = emailContextCaptor.getValue();
-//        Assertions.assertEquals("user@example.com", capturedEmailContext.getTo());
-//        Assertions.assertEquals(BEST_COOK_EMAIL, capturedEmailContext.getFrom());
-//        Assertions.assertEquals(Locale.ENGLISH, capturedEmailContext.getLocale());
-//        Assertions.assertEquals(VERIFICATION_SUBJECT, capturedEmailContext.getSubject());
-//        Assertions.assertEquals(TEMPLATE_LOCATION, capturedEmailContext.getTemplateLocation());
-//        Assertions.assertEquals(2, capturedEmailContext.getVariables().size());
-//        Assertions.assertEquals(TEST_USER_FIRSTNAME + " " + TEST_USER_LASTNAME,
-//                capturedEmailContext.getVariables().get("fullName"));
-//        Assertions.assertEquals(UriComponentsBuilder.fromHttpUrl(BASE_URL)
-//                .path("/users/register/verify").queryParam("token", secureTokenRepository.findById(1L).get().getToken()).toUriString(),
-//                capturedEmailContext.getVariables().get("verificationURL"));
-//
-//        Assertions.assertEquals(1, userRepository.count());
-//
-//        Optional<UserEntity> newlyCreatedUserOpt = userRepository.findByUsername(TEST_USER_USERNAME);
-//
-//        Assertions.assertTrue(newlyCreatedUserOpt.isPresent());
-//
-//        UserEntity newlyCreatedUser = newlyCreatedUserOpt.get();
-//
-//        Assertions.assertEquals(TEST_USER_EMAIL, newlyCreatedUser.getEmail());
-//        Assertions.assertEquals(TEST_USER_FIRSTNAME, newlyCreatedUser.getFirstName());
-//        Assertions.assertEquals(TEST_USER_LASTNAME, newlyCreatedUser.getLastName());
-//        Assertions.assertTrue(passwordEncoder.matches(TEST_USER_PASSWORD, newlyCreatedUser.getPassword()));
-//        Assertions.assertFalse(newlyCreatedUser.isAccountVerified());
+        String verificationUrl = UriComponentsBuilder.fromHttpUrl(BASE_URL)
+                .path("/users/register/verify")
+                .queryParam("token", secureTokenRepository.findById(1L).get().getToken()).toUriString();
+
+        Assertions.assertTrue(GreenMailUtil
+                .getBody(verificationEmail)
+                .contains(verificationUrl));
+
+        Assertions.assertEquals(1, verificationEmail.getAllRecipients().length);
+        Assertions.assertEquals(TEST_USER_EMAIL, verificationEmail.getAllRecipients()[0].toString());
+        Assertions.assertEquals(BEST_COOK_EMAIL, verificationEmail.getFrom()[0].toString());
+
+        Assertions.assertEquals(1, userRepository.count());
+
+        Optional<UserEntity> newlyCreatedUserOpt = userRepository.findByUsername(TEST_USER_USERNAME);
+
+        Assertions.assertTrue(newlyCreatedUserOpt.isPresent());
+
+        UserEntity newlyCreatedUser = newlyCreatedUserOpt.get();
+
+        Assertions.assertEquals(TEST_USER_EMAIL, newlyCreatedUser.getEmail());
+        Assertions.assertEquals(TEST_USER_FIRSTNAME, newlyCreatedUser.getFirstName());
+        Assertions.assertEquals(TEST_USER_LASTNAME, newlyCreatedUser.getLastName());
+        Assertions.assertTrue(passwordEncoder.matches(TEST_USER_PASSWORD, newlyCreatedUser.getPassword()));
+        Assertions.assertFalse(newlyCreatedUser.isAccountVerified());
     }
 
-//    @Test
-//    void testRegistrationFail() throws Exception {
-//        mockMvc.perform(post("/users/register").
-//                        param("username", TEST_USER_USERNAME).
-//                        param("firstName", " ").
-//                        param("lastName", TEST_USER_LASTNAME).
-//                        param("email", TEST_USER_EMAIL).
-//                        param("password", TEST_USER_PASSWORD).
-//                        param("confirmPassword", TEST_USER_PASSWORD).
-//                        cookie(new Cookie("lang", Locale.ENGLISH.getLanguage())).
-//                        with(csrf())
-//                )
-//                .andExpect(status().is3xxRedirection())
-//                .andExpect(redirectedUrl("/users/register"));
-//        verify(mockEmailService, never()).sendEmail();
-//    }
+    @Test
+    void testRegistrationFail() throws Exception {
+        mockMvc.perform(post("/users/register").
+                        param("username", TEST_USER_USERNAME).
+                        param("firstName", " ").
+                        param("lastName", TEST_USER_LASTNAME).
+                        param("email", TEST_USER_EMAIL).
+                        param("password", TEST_USER_PASSWORD).
+                        param("confirmPassword", TEST_USER_PASSWORD).
+                        cookie(new Cookie("lang", Locale.ENGLISH.getLanguage())).
+                        with(csrf())
+                )
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/users/register"));
+    }
 }
